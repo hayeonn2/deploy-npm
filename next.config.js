24bb793289cb169987dfb607f6126ab9f8b589cd
hayoenn2 <dwd1205@naver.com>
2023-08-03 00:01:42 +0900

    swc commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
-  images: {
-    domains: ["firebasestorage.googleapis.com", "www.gravatar.com"],
-  },
-  webpack: config => {
-    config.resolve.alias["@"] = path.join(__dirname, "app");
-    return config;
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  // experimental: {
+  //   appDir: true,
+  // },
+  // distDir: "build",
+  // images: {
+  //   domains: ["firebasestorage.googleapis.com", "www.gravatar.com"],
+  // },
+  // webpack: config => {
+  //   config.module.rules.push({
+  //     test: /\.svg$/i,
+  //     issuer: /\.[jt]sx?$/,
+  //     use: ["@svgr/webpack"],
+  //   });
+  //   config.resolve.alias["@"] = path.join(__dirname, "app");
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
