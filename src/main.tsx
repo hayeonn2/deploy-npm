5f53beb85383a4a380d09ce2880f14198acae05b
hayoenn2 <dwd1205@naver.com>
2023-08-03 20:38:47 +0900

    webpack test

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import './styles/main.css'
-
-import App from "./App"
-
-const rootElem = document.getElementById('app');
-ReactDOM.render(<App />, rootElem);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+};
